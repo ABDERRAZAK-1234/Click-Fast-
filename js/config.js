c739fca0f5282c40f44578d4206e8040d8efddeb
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-09-10 14:28:52 +0100

    j'ai ajouter des modifications pour verifier est que le button est submitted pour faire des autres chauses

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
+let sectionResult = document.getElementById("view-results");
 // let sectionConfig = document.getElementById('view-config');
 let sectionGame = document.getElementById('view-game');
 let formConfig = document.getElementById("config-form");
@@ -10,50 +11,79 @@ let choix = {
     duree : null,
     difficulte : null
 };
+let sec = 0 ;
+let submitted = false;
+let duree = 0;
+
+let timer = document.getElementById("timer");
 
 formConfig.addEventListener('submit', function(event){
     event.preventDefault();
-
+    
+    
     let pseudoValue = document.getElementById("pseudo").value;
     
     if(choix.mode === null || choix.duree === null || choix.difficulte === null){
         alert("Veuillez choisir toutes les options");
         return;
     }
+    submitted = true;
     // console.log(pseudoValue);
     localStorage.setItem('pseudo',pseudoValue);
     localStorage.setItem('choix',JSON.stringify(choix));
     
     sectionConfig.hidden = true;
-
+    
     let countdownSection = document.getElementById("view-countdown");
     let countdown = document.getElementById("countdown");
-
+    
     countdownSection.hidden = false;
-
-let seconds = 3;
-countdown.textContent = seconds;
-
-let timerCountdown = setInterval(function() {
-    seconds--;
-
+    
+    let seconds = 3;
     countdown.textContent = seconds;
-
+    
+    let timerCountdown = setInterval(function() {
+        seconds--;
+        
+        countdown.textContent = seconds;
+        
     if (seconds === 0) {
         clearInterval(timerCountdown);
-
+        
         countdownSection.hidden = true;
         sectionGame.hidden = false;
+        
+        sec = dureeSec;
+        // console.log(typeof(sec));
+        // console.log(sec);
+        
+        
+        let timeOfGame = setInterval(function(){
+            sec--;
+            
+            timer.textContent = sec
+            
+            checkSubmetted();
+            if(sec === 0){
+                clearInterval(timeOfGame);
+
+        // sectionGame.hidden = true;
+        // sectionResult.hidden = false;
+    }
+
+},1000);
     }
 }, 1000);
 
-    let timer = document.getElementById("timer");
 
 let choix2 = JSON.parse(localStorage.getItem("choix"));
-let dureeSec = choix2.duree;
-console.log(dureeSec);
+dureeSec = Number(choix2.duree);
+// console.log(dureeSec);
 
 timer.textContent = dureeSec;
+
+
+
 });
 optionGroups.forEach(function(group) {
 
@@ -80,14 +110,3 @@ optionGroups.forEach(function(group) {
     });
     
 });
-
-
-// setTimeout(() => {
-//     console.log(choix);
-    
-// }, 3000);
-
-// console.log(opt);
-
-
-
