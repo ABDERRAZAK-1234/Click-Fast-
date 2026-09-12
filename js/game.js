cd360539cc872dae8dbfb06f17b7ea421f8e0ae0
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-09-12 11:52:12 +0100

    J'ai ajouter la logic de mode precision et la logic de mode defi et le déplacement automatique de Target

--- js/game.js
+++ js/game.js
@@ -1,12 +1,14 @@
-let gameArea = document.getElementById("game-arena")
+let gameArea = document.getElementById("game-arena");
 let target = document.getElementById("target");
 let gameScore = document.getElementById("score");
 
 let score = 0;
 
+let bestScore = Number(localStorage.getItem("bestScore")) || 0;
 
-target.addEventListener("click", function () {
+let defiTimer;
 
+function moveTarget() {
     let maxX = gameArea.clientWidth - target.offsetWidth;
     let maxY = gameArea.clientHeight - target.offsetHeight;
 
@@ -16,25 +18,49 @@ target.addEventListener("click", function () {
     target.style.left = x + "px";
     target.style.top = y + "px";
 
-    score++;
-    gameScore.textContent = score;
-    localStorage.setItem("score",score);
-    
-});
-
-
-function checkSubmetted() {
-
-    if (submitted) {
-        // console.log(sec);
-        
-        if(sec == 0){
-            sectionResult.hidden = false;
-            sectionGame.hidden = true;
-            checkGameFinished();
-        }
+    let choix2 = JSON.parse(localStorage.getItem("choix"));
+    if (choix2.mode === "Defi") {
+        modeDefi();
     }
 }
-// checkSubmetted();
 
+target.addEventListener("click", function () {
 
+    score++;
+
+    gameScore.textContent = score;
+
+    localStorage.setItem("score", score);
+
+    if (score > bestScore) {
+        bestScore = score;
+        localStorage.setItem("bestScore", bestScore);
+    }
+
+    // moveTarget();
+
+});
+// Mode Défi
+function modeDefi() {
+
+    clearTimeout(defiTimer);
+
+    defiTimer = setTimeout(function () {
+        moveTarget();
+    }, 1000);
+
+}
+
+// mode precision
+gameArea.addEventListener("click", function (event) {
+
+    let choix2 = JSON.parse(localStorage.getItem("choix"));
+
+    if (choix2.mode === "Precision" && event.target !== target) {
+        score--;
+
+        gameScore.textContent = score;
+
+        localStorage.setItem("score", score);
+    }
+});
